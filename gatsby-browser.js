7c808729a7cb5691a02ae806d418cca295774c8d
m/s <m@umbr-ch.de>
2026-07-01 17:20:56 +0200

    gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,34 +3,9 @@
  *
  * See: https://www.gatsbyjs.com/docs/browser-apis/
  */
-// import React from "react"
-// import { CtxProvider } from "./src/context/AppContext"
-// import { InstantSearch } from "react-instantsearch"
-// import {
-//   searchClient,
-//   indexName,
-//   routing,
-// } from "./src/utils/algolia-search-config"
-// require("./src/assets/css/global.css")
-
-// export const wrapPageElement = ({ element, props }) => (
-//   <InstantSearch
-//     searchClient={searchClient}
-//     indexName={indexName}
-//     routing={routing}
-//     future={{ preserveSharedStateOnUnmount: true }}
-//   >
-//     {element}
-//   </InstantSearch>
-// )
-
-// export const wrapRootElement = ({ element }) => (
-//   <CtxProvider>{element}</CtxProvider>
-// )
-
-// gatsby-browser.js
-import React, { lazy, Suspense } from "react"
+import React from "react"
 import { CtxProvider } from "./src/context/AppContext"
+import { InstantSearch } from "react-instantsearch"
 import {
   searchClient,
   indexName,
@@ -38,21 +13,15 @@ import {
 } from "./src/utils/algolia-search-config"
 require("./src/assets/css/global.css")
 
-const InstantSearch = lazy(() =>
-  import("react-instantsearch").then(mod => ({ default: mod.InstantSearch })),
-)
-
 export const wrapPageElement = ({ element, props }) => (
-  <Suspense fallback={element}>
-    <InstantSearch
-      searchClient={searchClient}
-      indexName={indexName}
-      routing={routing}
-      future={{ preserveSharedStateOnUnmount: true }}
-    >
-      {element}
-    </InstantSearch>
-  </Suspense>
+  <InstantSearch
+    searchClient={searchClient}
+    indexName={indexName}
+    routing={routing}
+    future={{ preserveSharedStateOnUnmount: true }}
+  >
+    {element}
+  </InstantSearch>
 )
 
 export const wrapRootElement = ({ element }) => (
